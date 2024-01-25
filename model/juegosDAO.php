6a8929239dca8e72a0d3619a233e17aa86e04cec
Andreu <andreu@prueba.com>
2024-01-25 20:55:04 +0100

    creacion de carpeta model con archivos Juego y Usuario.php y JuegosDAO.php para operaciones con BD

---
+++ model/juegosDAO.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'config/dataBase.php';
+
+require_once 'Juego.php';
+require_once 'Usuario.php';
+
+class JuegosDAO{
+    public static function obtenerTodosJuegos($categoria){
+
+    }
+}
+
+?>
