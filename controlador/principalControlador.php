8945301a50a09c39d4f461ff0f75966f7086ad5a
David <david@prueba.com>
2024-01-25 19:35:00 +0100

    Ultimo cambio

--- controlador/principalControlador.php
+++ controlador/principalControlador.php
@@ -13,7 +13,7 @@ class principalControlador {
         //include_once 'vista/header.php';
 
         // Main
-        //include_once 'vista/home.php';
+        include_once 'vista/home.php';
 
         // Footer
         //include_once 'vista/footer.php';
---
+++ vista/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC>
+<html lang="es">
+<head>
+    <title>Home - Aprende Jugando</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <link href="assets/css/home.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+<body>
+
+
+</body>
