ee32c13b663a0b2ad60156b31fd20dac38019398
David <david@prueba.com>
2024-01-25 16:05:52 +0100

    Definicion de base del proyecto parte 2

--- config/dataBase.php
+++ config/dataBase.php
@@ -1,7 +1,7 @@
 <?php
 // Funcion que devuelve la conexion con la base de datos con tan solo llamarla
 class DataBase {
-    public static function connect($host='localhost',$user='root',$pwd='',$db='bd_videojuegos') {
+    public static function connect($host='localhost',$user='root',$pwd='',$db='bd_aprendejugando') {
         $conexion = new mysqli($host,$user,$pwd,$db);
         if($conexion == false){
             die('DATABASE ERROR');
---
+++ controlador/principalControlador.php
@@ -0,0 +1,21 @@
+
+<?php
+// Introducir aqui los modelos a utilizar
+//Ejemplo : include_once 'modelo/productoDAO.php';
+
+// Declaracion del controlador principal
+class principalControlador {
+
+    // Funcion predeterminada del controlador
+    public function index() {
+
+        // Header
+        //include_once 'vista/header.php';
+
+        // Main
+        //include_once 'vista/home.php';
+
+        // Footer
+        //include_once 'vista/footer.php';
+    }
+}
